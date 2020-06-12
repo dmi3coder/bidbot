95ea6a97f2a1a49a2e599694db6f65a5a681c855
dmi3coder <dmi3coder@gmail.com>
2020-06-12 11:25:10 +0300

    Initial commit

---
+++ src/main/java/de/optimax/bidbot/BidbotApplication.java
@@ -0,0 +1,13 @@
+package de.optimax.bidbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/optimax/bidbot/BidbotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.optimax.bidbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
